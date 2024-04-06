2215af7cb0a50c2119ffd26060f7dabaaf29a9fb
Null <kelanidarasimi9@gmail.com>
2024-04-06 22:37:03 +0100

    fix error

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,36 @@
-$(document).ready(function () {
-    let checkedAmenities = {};
+// $(document).ready(function () {
+//   let checkedAmenities = {};
 
-    $(document).on('change', "input[type='checkbox']", function () {
-        if (this.checked) {
-            checkedAmenities[$(this).data('id') = $(this).data(('name'))];
-        } else {
-            delete checkedAmenities[$this().data('id')];
-        }
-        let lst = Object.values(checkedAmenities);
-        if (lst.length > 0) {
-            $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
-        } else {
-            $('div.amenities > h4').html('&nbsp;');
-        }
-    });
+//   $(document).on("change", "input[type='checkbox']", function () {
+//     if (this.checked) {
+//       checkedAmenities[$(this).data("id")] = $(this).data("name");
+//     } else {
+//       delete checkedAmenities[$(this).data("id")];
+//     }
+//     let lst = Object.values(checkedAmenities);
+//     if (lst.length > 0) {
+//       $("div.amenities > h4").text(lst.join(", "));
+//     } else {
+//       $("div.amenities > h4").html("&nbsp;");
+//     }
+//   });
+// });
+
+$(document).ready(function () {
+  let checkedAmenities = {};
+
+  $(document).on("change", "input[type='checkbox']", function () {
+    if (this.checked) {
+      checkedAmenities[$(this).data("id")] = $(this).data("name");
+    } else {
+      delete checkedAmenities[$(this).data("id")];
+    }
+    let lst = Object.values(checkedAmenities);
+    if (lst.length > 0) {
+        console.log(lst);
+      $("div.amenities > h4").text(lst.join(", "));
+    } else {
+      $("div.amenities > h4").html("&nbsp;");
+    }
+  });
 });
